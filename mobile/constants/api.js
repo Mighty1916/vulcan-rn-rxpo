27bd7f01d3a5a9e69567a2de99f847e1387be3e1
Albab khan <albabkhan1916135@gmail.com>
2025-07-17 17:37:47 +0530

    preview

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://vulcan-rn-rxpo-3.onrender.com/api/orders";
-API_URL = "https://vulcan-rn-rxpo-3.onrender.com/api/apply";
+export const ORDERS_API_URL = "https://vulcan-rn-rxpo-3.onrender.com/api/orders";
+export const APPLY_API_URL = "https://vulcan-rn-rxpo-3.onrender.com/api/apply";
