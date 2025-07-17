886b47e3091e9ff61e0ee51e181c978da03337e0
Albab khan <albabkhan1916135@gmail.com>
2025-07-17 16:04:12 +0530

    Add cron.js

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,1 +1,2 @@
-export const API_URL = "http:localhost:3000/api";
+export const API_URL = "https://vulcan-rn-rxpo-3.onrender.com/api/orders";
+API_URL = "https://vulcan-rn-rxpo-3.onrender.com/api/apply";
